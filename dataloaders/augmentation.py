2f2317a43a58c7cabdd7366e2cba4c6c52a580c0
yuyang <xigua7830@gmail.com>
2025-07-18 14:43:11 +0800

    fix: fix a bug in RandomCrop augmentation

--- dataloaders/augmentation.py
+++ dataloaders/augmentation.py
@@ -46,8 +46,7 @@ def median_blur(input: torch.Tensor, kernel_size: tuple) -> torch.Tensor:
         raise TypeError(f"Input type is not a torch.Tensor. Got {type(input)}")
 
     if not len(input.shape) == 3:
-        raise ValueError(
-            f"Invalid input shape, we expect CxHxW. Got: {input.shape}")
+        raise ValueError(f"Invalid input shape, we expect CxHxW. Got: {input.shape}")
 
     padding = _compute_zero_padding(kernel_size)
 
@@ -56,10 +55,9 @@ def median_blur(input: torch.Tensor, kernel_size: tuple) -> torch.Tensor:
     c, h, w = input.shape
 
     # map the local window to single vector
-    features: torch.Tensor = F.conv2d(input.reshape(c, 1, h, w),
-                                      kernel,
-                                      padding=padding,
-                                      stride=1)
+    features: torch.Tensor = F.conv2d(
+        input.reshape(c, 1, h, w), kernel, padding=padding, stride=1
+    )
     features = features.view(c, -1, h, w)
 
     median: torch.Tensor = torch.median(features, dim=1)[0]
@@ -122,7 +120,7 @@ class Compose:
 
 class PermuteToCHW(object):
 
-    def __init__(self, apply_on=['input']) -> None:
+    def __init__(self, apply_on=["input"]) -> None:
         self.apply_on = set(apply_on)
 
     def __call__(self, datas: dict):
@@ -148,10 +146,7 @@ class ToTensor(object):
 
 class RandomCrop(object):
 
-    def __init__(self,
-                 size: Union[int | float],
-                 ratio=0.5,
-                 apply_on=['input']):
+    def __init__(self, size: Union[int | float], ratio=0.5, apply_on=["input"]):
         self.crop_size = size
         self.ratio = ratio
         self.apply_on = set(apply_on)
@@ -164,14 +159,17 @@ class RandomCrop(object):
                 data = datas[k]
                 ori_h, ori_w = data.shape[-2:]
                 if self.crop_size < 1:
-                    crop_size = (int(self.crop_size * ori_h),
-                                 int(self.crop_size * ori_w))
+                    crop_size = (
+                        int(self.crop_size * ori_h),
+                        int(self.crop_size * ori_w),
+                    )
                 else:
                     crop_size = (self.crop_size, self.crop_size)
-                if ori_h > self.crop_size or ori_w > self.crop_size:
+                if ori_h < self.crop_size or ori_w < self.crop_size:
                     return datas
                 i, j, h, w = transforms.RandomCrop.get_params(
-                    data, output_size=crop_size)
+                    data, output_size=crop_size
+                )
                 break
             for k, v in datas.items():
                 if k in self.apply_on and v is not None:
@@ -182,11 +180,7 @@ class RandomCrop(object):
 
 class RandomFlip(object):
 
-    def __init__(self,
-                 ratio=0.5,
-                 vertical=True,
-                 horizontal=True,
-                 apply_on=['input']):
+    def __init__(self, ratio=0.5, vertical=True, horizontal=True, apply_on=["input"]):
         self.ratio = ratio
         self.vertical = vertical
         self.horizontal = horizontal
@@ -209,7 +203,7 @@ class RandomFlip(object):
 
 class Random90Rotate(object):
 
-    def __init__(self, ratio=0.5, apply_on=['input']):
+    def __init__(self, ratio=0.5, apply_on=["input"]):
         self.ratio = ratio
         self.apply_on = set(apply_on)
 
@@ -224,7 +218,7 @@ class Random90Rotate(object):
 
 class Resize(object):
 
-    def __init__(self, size, apply_on=['input'], clip=True):
+    def __init__(self, size, apply_on=["input"], clip=True):
         assert isinstance(size, (int, tuple, dict))
         if isinstance(size, int):
             self.size = (size, size)
@@ -239,9 +233,8 @@ class Resize(object):
                 size = list(self.size)
                 if size[0] > 1 and size[1] > 1:
                     datas[k] = TF.resize(
-                        datas[k],
-                        size,
-                        interpolation=TF.InterpolationMode.BICUBIC)
+                        datas[k], size, interpolation=TF.InterpolationMode.BICUBIC
+                    )
                     if self.clip:
                         datas[k] = torch.clamp(datas[k], 0, 1)
         return datas
@@ -249,11 +242,7 @@ class Resize(object):
 
 class RandomResize(object):
 
-    def __init__(self,
-                 default_size,
-                 target_size,
-                 ratio=0.2,
-                 apply_on=['input']):
+    def __init__(self, default_size, target_size, ratio=0.2, apply_on=["input"]):
         self.ratio = ratio
         assert isinstance(default_size, (int, tuple, dict))
         if isinstance(default_size, int):
@@ -268,19 +257,19 @@ class RandomResize(object):
         self.apply_on = set(apply_on)
 
     def __call__(self, datas: dict):
-        size = self.target_size if random.random(
-        ) < self.ratio else self.default_size
+        size = self.target_size if random.random() < self.ratio else self.default_size
 
         for k, v in datas.items():
             if k in self.apply_on and v is not None:
                 datas[k] = TF.resize(
-                    datas[k], size, interpolation=TF.InterpolationMode.BICUBIC)
+                    datas[k], size, interpolation=TF.InterpolationMode.BICUBIC
+                )
         return datas
 
 
 class RandomCropResize(object):
 
-    def __init__(self, ratio=0.5, apply_on=['input']):
+    def __init__(self, ratio=0.5, apply_on=["input"]):
         self.ratio = ratio
         self.apply_on = set(apply_on)
 
@@ -294,25 +283,27 @@ class RandomCropResize(object):
                 data = datas[k]
                 if data is not None:
                     shape = (data.shape[-2], data.shape[-1])
-                    crop_size = (int(shape[0] * scale_ratio),
-                                 int(shape[1] * scale_ratio))
+                    crop_size = (
+                        int(shape[0] * scale_ratio),
+                        int(shape[1] * scale_ratio),
+                    )
                     i, j, h, w = transforms.RandomCrop.get_params(
-                        data, output_size=crop_size)
+                        data, output_size=crop_size
+                    )
                     break
 
             for k, v in datas.items():
                 if k in self.apply_on and v is not None:
                     datas[k] = TF.crop(datas[k], i, j, h, w)
                     datas[k] = TF.resize(
-                        datas[k],
-                        shape,
-                        interpolation=TF.InterpolationMode.BICUBIC)
+                        datas[k], shape, interpolation=TF.InterpolationMode.BICUBIC
+                    )
         return datas
 
 
 class RandomRotate(object):
 
-    def __init__(self, ratio=0.5, ranges=30, apply_on=['input']):
+    def __init__(self, ratio=0.5, ranges=30, apply_on=["input"]):
         self.ratio = ratio
         self.ranges = ranges
         self.apply_on = set(apply_on)
@@ -324,25 +315,21 @@ class RandomRotate(object):
                 if k in self.apply_on and v is not None:
                     datas[k] = kornia.geometry.rotate(
                         datas[k],
-                        torch.tensor(angle,
-                                     dtype=datas[k].dtype,
-                                     device=datas[k].device),
-                        padding_mode='reflection')
+                        torch.tensor(
+                            angle, dtype=datas[k].dtype, device=datas[k].device
+                        ),
+                        padding_mode="reflection",
+                    )
         return datas
 
 
 class RandomIllumination(object):
 
-    def __init__(self,
-                 candidate_illums,
-                 ratio=0.5,
-                 radius=0.02,
-                 mix_ratio=0.5):
+    def __init__(self, candidate_illums, ratio=0.5, radius=0.02, mix_ratio=0.5):
         self.ratio = ratio
         self.radius = radius
         if not isinstance(candidate_illums, torch.Tensor):
-            candidate_illums = torch.tensor(candidate_illums,
-                                            dtype=torch.float32)
+            candidate_illums = torch.tensor(candidate_illums, dtype=torch.float32)
         self.candidate_illums = candidate_illums
         self.mix_ratio = mix_ratio
 
@@ -357,8 +344,7 @@ class RandomIllumination(object):
                 index = random.randint(0, len(self.candidate_illums) - 1)
                 illum = self.candidate_illums[index]
             delta_r = random.uniform(0, self.radius) * random.choice([-1, 1])
-            delta_b = math.sqrt(self.radius**2 - delta_r**2) * random.choice(
-                [-1, 1])
+            delta_b = math.sqrt(self.radius**2 - delta_r**2) * random.choice([-1, 1])
             rg = illum[0]
             bg = illum[-1]
             rg += delta_r
@@ -366,13 +352,13 @@ class RandomIllumination(object):
             rg = np.clip(rg, 0.01, None)
             bg = np.clip(bg, 0.01, None)
 
-            ori_illum = datas['illum']
-            ori_input = datas['input']
+            ori_illum = datas["illum"]
+            ori_input = datas["input"]
             ori_input[0] *= rg / ori_illum[0]
             ori_input[-1] *= bg / ori_illum[-1]
             ori_input = torch.clamp(ori_input, 0, 1)
-            datas['illum'] = torch.tensor([rg, 1., bg], dtype=torch.float32)
-            datas['input'] = ori_input.float()
+            datas["illum"] = torch.tensor([rg, 1.0, bg], dtype=torch.float32)
+            datas["input"] = ori_input.float()
 
         return datas
 
@@ -388,21 +374,19 @@ class RandomNearbyIllumination(object):
         self.ratio = ratio
         self.radius = radius
         if not isinstance(candidate_illums, torch.Tensor):
-            candidate_illums = torch.tensor(candidate_illums,
-                                            dtype=torch.float32)
+            candidate_illums = torch.tensor(candidate_illums, dtype=torch.float32)
         self.candidate_illums = candidate_illums
 
     def __call__(self, datas: dict):
         if random.random() < self.ratio:
-            ori_illum = datas['illum']
-            ori_input = datas['input']
+            ori_illum = datas["illum"]
+            ori_input = datas["input"]
             dist = torch.norm(ori_illum - self.candidate_illums, dim=1)
             indexes = torch.argsort(dist)[:3]
             candidate_illums = self.candidate_illums[indexes]
             illum = random.choice(candidate_illums)
             delta_r = random.uniform(0, self.radius) * random.choice([-1, 1])
-            delta_b = math.sqrt(self.radius**2 - delta_r**2) * random.choice(
-                [-1, 1])
+            delta_b = math.sqrt(self.radius**2 - delta_r**2) * random.choice([-1, 1])
             rg = illum[0]
             bg = illum[-1]
             rg += delta_r
@@ -413,15 +397,15 @@ class RandomNearbyIllumination(object):
             ori_input[0] *= rg / ori_illum[0]
             ori_input[-1] *= bg / ori_illum[-1]
             ori_input = torch.clamp(ori_input, 0, 1)
-            datas['illum'] = torch.tensor([rg, 1., bg], dtype=torch.float32)
-            datas['input'] = ori_input.float()
+            datas["illum"] = torch.tensor([rg, 1.0, bg], dtype=torch.float32)
+            datas["input"] = ori_input.float()
 
         return datas
 
 
 class RandomNoise(object):
 
-    def __init__(self, ratio=0.5, std=0.01, apply_on=['input']):
+    def __init__(self, ratio=0.5, std=0.01, apply_on=["input"]):
         self.ratio = ratio
         self.std = std
         self.apply_on = set(apply_on)
